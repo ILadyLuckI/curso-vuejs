033eb6e54e51f48f71c7b68074ff4896597c3c39
Daiana Koblitz <69736274+ILadyLuckI@users.noreply.github.com>
2023-08-03 14:30:09 -0300

    Arquivos 'formulario-desafio-src' adicionados

---
+++ formulario-desafio-src/App.vue
@@ -0,0 +1,150 @@
+<template>
+	<div id="app">
+		<h1>Formulário Desafio</h1>
+		<div class="conteudo">
+			<form class="painel" v-if="!enviado">
+				<div class="cabecalho">Formulário</div>
+				<!--Registrando NomeCompleto-->
+				<NomeCompleto v-model="nomeCompleto" />
+				<!-- Exercicio 01 -->
+				<!-- Criar uma formulário de registro -->
+				<!-- Nome completo (Nome e Sobrenome) -->
+				<!-- Email -->
+				<!-- Senha -->
+				<!-- Armazenar Dados? (Sim/Não) -->
+			
+				
+				<Rotulo nome="email">
+					<input type="email" v-model="email">
+				</Rotulo>
+				<Rotulo nome="Senha">
+					<input type="password" v-model="senha">
+				</Rotulo>
+				<Rotulo nome="Armazenar Dados?">
+					<input type="checkbox" v-model="armazenarDados">
+				</Rotulo>
+
+				<!-- Exercicio 02 -->
+				<!-- Só mostrar o fomulário de não tiver sido submetido -->
+				<!-- Mostrar a área de Resultado apenas quando o formulário for submetido -->
+				<hr>
+				<button @click.prevent="enviar">Enviar</button>
+				
+				<!-- Exercicio 03 -->
+				<!-- Crie um componente personalizado NomeCompleto -->
+				<!-- Esse componente deve receber Nome e Sobrenome -->
+			</form>
+			<!--Interpolando os dados-->
+			<div class="painel" v-else>
+				<div class="cabecalho">Resultado</div>
+				<Rotulo nome="Nome">
+					<span>{{ nomeCompleto.nome }}</span>
+				</Rotulo>
+				<Rotulo nome="Sobrenome">
+					<span>{{ nomeCompleto.sobrenome }}</span>
+				</Rotulo>
+				<Rotulo nome="Email">
+					<span>{{ email }}</span>
+				</Rotulo>
+				<Rotulo nome="Senha">
+					<span>{{ senha }}</span>
+				</Rotulo>
+				<Rotulo nome="Armazenar Dados?">
+					<span>{{ armazenarDados }}</span>
+				</Rotulo>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+import Rotulo from './components/Rotulo.vue'
+/*Importando componente personalizado NomeCompleto*/
+import NomeCompleto from './components/NomeCompleto.vue'
+
+
+export default {
+	name: 'app',
+	/* Registrando Rotulo e NomeCompleto*/
+	components: { Rotulo, NomeCompleto },
+	/*metodo data, que retorna um objeto*/
+	data() {
+		return {
+			enviado: false,
+			/*o atributo nomeCompleto será o v-model do componente <NomeCompleto>*/
+			nomeCompleto: {
+				nome: '',
+				sobrenome: '',
+			},
+			email: '',
+			senha: '',
+			armazenarDados: true,
+		}
+	},
+	methods: {
+		enviar() {
+			this.enviado = true
+		}
+	}
+}
+</script>
+
+<style>
+
+body {
+	background-color: #ffe4fb;
+}
+
+#app {
+	font-family: 'Avenir', Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+
+	display: flex;
+	flex-direction: column;
+}
+
+.conteudo {
+	display: flex;
+}
+
+.painel {
+	flex: 1;
+	background: #FFF;
+	margin: 0px 10px;
+	padding: 20px;
+	border: 1px solid #AAA;
+	border-radius: 5px;
+}
+
+.painel .cabecalho {
+	width: 100%;
+	background-color: #ff05ea;
+	padding: 10px 0px;
+	border-radius: 5px;
+	font-size: 1.4rem;
+	color: white;
+}
+
+#app form button {
+	float: right;
+	margin: 10px 0px;
+	padding: 10px 20px;
+	font-size: 1.4rem;
+	border-radius: 5px;
+	color: #FFF;
+	background-color: #ff05ea;
+}
+
+#app h1 {
+	font-weight: 200;
+	margin: 20px;
+	padding: 0;
+}
+
+.mr-4 {
+	margin-right: 40px;
+}
+</style>
---
+++ formulario-desafio-src/components/NomeCompleto.vue
@@ -0,0 +1,47 @@
+<!--Criando componente personalizado-->
+<template>
+    <div>
+        <Rotulo nome="Nome">
+			<input type="text" @input="alterarNome"
+                :value="value.nome">
+			</Rotulo>
+		<Rotulo nome="Sobrenome">
+			<input type="text" @input="alterarSobrenome"
+                :value="value.sobrenome">
+		</Rotulo>
+    </div>
+</template>
+
+<script>
+import Rotulo from './Rotulo.vue'
+
+export default {
+    /*Iremos passar um objeto para que esse componente funcione*/
+    props: {
+        value: {
+            type: Object,
+            required: true
+        }
+    },
+    components: { Rotulo },
+    methods: {
+        alterarNome(event) {
+            this.$emit('input', { 
+                nome: event.target.value, 
+                sobrenome: this.value.sobrenome
+             })
+        },
+        alterarSobrenome(event) {
+            this.$emit('input', { 
+                /*o this.value.nome é recebido a partir das props*/
+                nome: this.value.nome, 
+                sobrenome: event.target.value
+            })
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
