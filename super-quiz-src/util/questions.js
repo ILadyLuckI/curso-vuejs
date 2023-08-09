642f66dc1266a8790d8d7a5909bf371f501e2432
Daiana Koblitz <69736274+ILadyLuckI@users.noreply.github.com>
2023-08-09 11:57:46 -0300

    Pasta 'super-quiz-src' adicionada

---
+++ super-quiz-src/util/questions.js
@@ -0,0 +1,67 @@
+export default [
+    {
+        text: 'Qual é o melhor jogo?',
+        answers: [
+            { text: 'FORTNITE', correct: true },
+            { text: 'DEAD BY DAYLIGHT', correct: false },
+            { text: 'CS-GO', correct: false },
+            { text: 'OVERCOOKED', correct: false }
+        ]
+    },
+    {
+        text: 'Quem é o jogador mais poderoso do Salve o Mundo no seu grupo de amigues webs?',
+        answers: [
+            { text: 'LADYLUCK', correct: true },
+            { text: 'NATH', correct: false },
+            { text: 'LENIS', correct: false },
+            { text: 'ARI', correct: false },
+            { text: 'IZABELLA', correct: false}
+        ]
+    },
+    {
+        text: 'Quem é o melhor jogador no seu grupo de amigues webs?',
+        answers: [
+            { text: 'LADYLUCK', correct: true },
+            { text: 'NATH', correct: false },
+            { text: 'LENIS', correct: false },
+            { text: 'ARI', correct: false },
+            { text: 'IZABELLA', correct: false}
+        ]
+    },
+    {
+        text: 'Quem grita mais no jogo?',
+        answers: [
+            { text: 'IZABELLA', correct: false },
+            { text: 'LENIS', correct: true },
+            { text: 'NATH', correct: false },
+            { text: 'ARI', correct: false }
+        ]
+    },
+    {
+        text: 'E o player mais ruim do jogo no seu grupo de amigues?',
+        answers: [
+            { text: 'LENIS', correct: false },
+            { text: 'NEUTRA', correct: false },
+            { text: 'ARI', correct: false },
+            { text: 'NATH', correct: true }
+        ]
+    },
+    {
+        text: 'E o segundo player mais ruim?',
+        answers: [
+            { text: 'NEUTRA', correct: false },
+            { text: 'ARI', correct: false },
+            { text: 'IZABELLA', correct: true },
+            { text: 'NATH', correct: false }
+        ]
+    },
+    {
+        text: 'Qual a melhor arma do Fortnite?',
+        answers: [
+            { text: 'SMG', correct: false },
+            { text: 'SNIPER 12', correct: false },
+            { text: 'PISTOLA MITICA', correct: true },
+            { text: 'PODERZINHO DA TEMPORADA', correct: false }
+        ]
+    },
+]
